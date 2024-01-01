b6c57f887a1461e68bcb99914d8ebf5e95460730
Michael Chen <michaelchen@Michaels-MacBook-Air.local>
2024-01-01 18:39:16 -0500

    Add Github Actions

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
